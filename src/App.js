69c555f64c0a658ee9779876a3f05a422ebe1029
Beringer Dorottya <beringerdorottya@gmail.com>
2023-11-21 17:53:15 +0100

    first version of app, experiment with marked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import './App.css';
+import {parse} from 'marked';
+
+
+const defaultText = '' //see userStory #5
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: defaultText
+    }
+    this.handleText = this.handleText.bind(this);
+  }
+
+  handleText() {
+    return this.setState(
+      {text: ''}
+    );
+  }
+
+  
+  render() {
+    return (
+      <div id="body-div">
+        <textarea id="editor" onChange={this.handleText}></textarea>
+        <div id="preview" dangerouslySetInnerHTML={{__html: parse('# Marked in browser\n\nRendered by **marked**.')}}>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
